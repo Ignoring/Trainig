e63db3b3794a4257b00906f50f04528317d964db
Fedoryn Igor <ifedoryn@amcbridge.com>
2016-05-15 12:59:13 +0300

    Create

---
+++ Training/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Training
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.File = new System.Windows.Forms.ToolStripMenuItem();
+            this.Options = new System.Windows.Forms.ToolStripMenuItem();
+            this.Help = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.About = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File,
+            this.Options,
+            this.Help});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(837, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // File
+            // 
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(37, 20);
+            this.File.Text = "File";
+            // 
+            // Options
+            // 
+            this.Options.Name = "Options";
+            this.Options.Size = new System.Drawing.Size(61, 20);
+            this.Options.Text = "Options";
+            // 
+            // Help
+            // 
+            this.Help.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.HelpHelp,
+            this.About});
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(44, 20);
+            this.Help.Text = "Help";
+            // 
+            // HelpHelp
+            // 
+            this.HelpHelp.Name = "HelpHelp";
+            this.HelpHelp.Size = new System.Drawing.Size(107, 22);
+            this.HelpHelp.Text = "Help";
+            // 
+            // About
+            // 
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(107, 22);
+            this.About.Text = "About";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(837, 344);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem File;
+        private System.Windows.Forms.ToolStripMenuItem Options;
+        private System.Windows.Forms.ToolStripMenuItem Help;
+        private System.Windows.Forms.ToolStripMenuItem HelpHelp;
+        private System.Windows.Forms.ToolStripMenuItem About;
+    }
+}
+
---
+++ Training/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Training
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Windows.Forms;
+
+    static class Program
+    {
+        /// <summary>
+        /// Port for connect to MySql
+        /// </summary>
+        private const int portMySql = 3309;
+        /// <summary>
+        /// MySql server folder
+        /// </summary>
+        public static string MySqlFolder {get; set; }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Program.MySqlFolder = GetMySqlPath();
+            Program.MySql(); // Run MySql server
+            //Application.Run(new Form1());
+            Program.MySql(true); // Run MySql server
+        }
+
+        /// <summary>
+        /// Start MySql Server
+        /// </summary>
+        private static void MySql(bool runStop = false)
+        {
+            var process = new Process();
+            // Stop the process from opening a new window
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            // Setup executable and parameters
+            if (runStop)
+                process.StartInfo.FileName += string.Concat(GetMySqlPath(), "\\MySql\\bin\\", "mysqladmin.exe");
+            else
+                process.StartInfo.FileName = string.Concat(GetMySqlPath(), "\\MySql\\bin\\", "mysqld.exe");
+
+            if (runStop)
+                process.StartInfo.Arguments += string.Concat("--user=root --port=", portMySql.ToString(), " shutdown");
+            else
+                process.StartInfo.Arguments = string.Concat("--port=", portMySql.ToString(), "");
+
+            process.Start();
+        }
+
+        /// <summary>
+        /// Get full path to MySql folder
+        /// </summary>
+        /// <param name="path">Some path to MySql folder</param>
+        /// <returns>Path to MySql folder</returns>
+        private static string GetMySqlPath(string path = "")
+        {
+            var foldersFound = Directory.GetDirectories(AppDomain.CurrentDomain.BaseDirectory, "DataBases", SearchOption.AllDirectories);
+            if (foldersFound == null || foldersFound.Length == 0)
+                foldersFound = Directory.GetDirectories(Path.GetFullPath(@"..\..\..\"), "DataBases", SearchOption.AllDirectories);
+            return foldersFound != null && foldersFound.Length > 0 ? foldersFound[0] : "";
+        }
+    }
+}
